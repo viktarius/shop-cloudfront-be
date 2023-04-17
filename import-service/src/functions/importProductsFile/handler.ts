370aed4b3420f44e309a706559c615baf526f0fe
Viktar Belski <Viktar_Belski@epam.com>
2023-04-18 01:34:50 +0300

    feat: import-service and import request

---
+++ import-service/src/functions/importProductsFile/handler.ts
@@ -0,0 +1,31 @@
+import { S3 } from 'aws-sdk';
+import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from '@libs/api-gateway';
+
+const s3 = new S3({ region: 'us-east-1' });
+
+const BUCKET = 'electronic-lootbox-shop-uploade-storage'
+export const importProductsFile: ValidatedEventAPIGatewayProxyEvent<any> = async (event) => {
+    const fileName = event.queryStringParameters?.name;
+    const catalogPath = `uploaded/${fileName}`;
+
+    let statusCode = 200;
+    let body = {};
+
+    const params = {
+        Bucket: BUCKET,
+        Key: catalogPath,
+        Expires: 60,
+        ContentType: 'text/csv',
+    }
+
+    try {
+        body = await s3.getSignedUrlPromise('putObject', params);
+    } catch (error) {
+        console.error('Error appears: ');
+        console.error(error)
+        statusCode = 500;
+        body = error;
+    }
+
+    return formatJSONResponse(body, statusCode);
+}
