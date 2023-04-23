49f4b1659c017cc0a9bc5fdc4210563c498fdc9b
Viktar Belski <Viktar_Belski@epam.com>
2023-04-24 00:28:00 +0300

    fix: notification

--- products-service/src/functions/catalogBatchProcess/handler.ts
+++ products-service/src/functions/catalogBatchProcess/handler.ts
@@ -6,24 +6,28 @@ import { productsService } from '../../services/products.service';
 import { loggerService } from '../../services/logger.service';
 
 export const catalogBatchProcess = async (event: SQSEvent) => {
-    const sns: SNS = new SNS({ region: 'us-east-1' });
+    let data = [];
+    try {
+        data = event.Records.map(({ body }) => JSON.parse(body));
+        data = data.map((d) => ({ ...d, price: +d.price }))
+        await Promise.allSettled(data.map(d => productsService.createProduct(d)));
+    } catch (err) {
+        loggerService.logError('catalogBatchProcess -->> Error in save data to DB --->>> ', err);
+    }
 
     try {
-        const data = event.Records.map(({ body }) => JSON.parse(body));
-        await Promise.all(data.map(d => productsService.createProduct(d)))
+        const sns: SNS = new SNS({ region: 'us-east-1' });
 
-        sns.publish({
-            Subject: 'Data added to DB',
-            Message: JSON.stringify(data),
-            TopicArn: process.env.SNS,
-        }, (err) => {
-            if (err) {
-                loggerService.logError('catalogBatchProcess -->> Error occur in public to SNS --->>> ', err);
-            } else {
-                loggerService.log('Data successfully sent to SNS ::: ', data);
-            }
-        })
-    } catch(e) {
-        loggerService.logError('catalogBatchProcess -->> Error in save data to DB --->>> ', e);
+        await Promise.allSettled(data.map(d => {
+            return sns.publish({
+                Subject: 'Data added to DB',
+                Message: JSON.stringify(d),
+                TopicArn: process.env.SNS,
+            }).promise()
+        }));
+
+        loggerService.log('Data successfully sent to SNS ::: ', data);
+    } catch (err) {
+        loggerService.logError('catalogBatchProcess -->> Error occur in public to SNS --->>> ', err);
     }
 }
