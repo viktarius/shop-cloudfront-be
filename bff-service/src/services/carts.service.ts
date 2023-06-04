2aa16a50fe0ddc85bb9968c7bed40950a44db6eb
Viktar Belski <Viktar_Belski@epam.com>
2023-06-04 12:23:29 +0300

    cache service

--- bff-service/src/services/carts.service.ts
+++ bff-service/src/services/carts.service.ts
@@ -2,18 +2,36 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { IRequestHandler } from './request-handler.interface';
 import { HttpService } from '@nestjs/axios';
 import { map, Observable, of } from 'rxjs';
+import { CartsCacheService } from './carts-cache.service';
 
 @Injectable()
 export class CartsService implements IRequestHandler {
     private readonly url = 'http://viktarius-cart-api-dev.us-east-1.elasticbeanstalk.com/api/v3/carts';
 
-    constructor(private readonly httpService: HttpService) {}
+    constructor(
+        private readonly httpService: HttpService,
+        private cartsCacheService: CartsCacheService
+    ) {}
 
     public getRequestHandler(params: any): Observable<any> {
         if (params?.id) {
-            return this.httpService.get(`${ this.url }/${ params.id }`).pipe(map(res => res.data));
+            if (this.cartsCacheService.getCache(params?.id)) {
+                return of(this.cartsCacheService.getCache(params?.id));
+            } else {
+                return this.httpService.get(`${ this.url }/${ params.id }`).pipe(map(res => {
+                    this.cartsCacheService.save(res.data, params?.id);
+                    return res.data;
+                }));
+            }
         } else {
-            return this.httpService.get(this.url).pipe(map(res => res.data));
+            if (this.cartsCacheService.getCache()) {
+                return of(this.cartsCacheService.getCache());
+            } else {
+                return this.httpService.get(this.url).pipe(map(res => {
+                    this.cartsCacheService.save(res.data);
+                    return res.data;
+                }));
+            }
         }
     }
 
--- bff-service/src/services/services.module.ts
+++ bff-service/src/services/services.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 
 import { CartsService } from './carts.service';
+import { CartsCacheService } from './carts-cache.service';
 
 @Module({
     imports: [HttpModule],
-    providers: [CartsService],
+    providers: [CartsService, CartsCacheService],
     exports: [CartsService]
 })
 export class ServicesModule {}
